385124c3f5fb207463cd40a2e995e9f1a8eba63d
Astemir_Mamkhegov <astemir.mamkhegov@ru.axxonsoft.com>
2021-06-22 04:30:35 +0300

    #1 Add new Project - Delivery System.

---
+++ Delivery System/Delivery System/Courier.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeoCoordinatePortable;
+
+namespace Delivery_System
+{
+    public class Courier
+    {
+        private string name;
+        private string phoneNumber;
+        private double maximalWeight;
+
+        public Courier(string name, string phoneNumber, double maximalWeight)
+        {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.maximalWeight = maximalWeight;
+            IsAvailable = true;
+        }
+
+        public List<Order> Orders { get; set; } = new List<Order>();
+
+        public string Name => name;
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                phoneNumber = value;
+            }
+        }
+
+        public double MaximalWeight
+        {
+            get
+            {
+                return maximalWeight;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Incorrect weight");
+                
+                maximalWeight = value;
+            }
+        }
+
+        public bool IsAvailable { get; set; }
+
+        public GeoCoordinate CurrentLocation { get; set; }
+    }
+}
---
+++ Delivery System/Delivery System/Order.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeoCoordinatePortable;
+
+namespace Delivery_System
+{
+    public class Order
+    {
+        private string number;
+        private double weight;
+        private DateTime? startDate;
+        private DateTime? finishDate;
+
+        public Order(string number, double weight)
+        {
+            this.number = number;
+            this.weight = weight;
+        }
+
+        public Courier Courier { get; set; }
+
+        public string Number => number;
+
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if (Courier != null)
+                    throw new Exception("Not change weight, if courier is assigned");
+
+                if (value < 0)
+                    throw new Exception("Incorrect weight");
+
+                weight = value;
+            }
+        }
+
+        public GeoCoordinate StartLocation { get; set; }
+        public GeoCoordinate FinishLocation { get; set; }
+    }
+}
